34cc3cedfd6cb0092ea39ec41a50ba7d0b625460
OlaniyiHope <olaniyihoppee@gmail.com>
2024-03-11 10:22:25 +0100

    all

--- src/pages/Land.js
+++ src/pages/Land.js
@@ -65,38 +65,6 @@ const Land = () => {
   return (
     <>
       <body>
-        <div class="color-switcher" id="choose_color">
-          {" "}
-          <a href="#." class="picker_close">
-            <i class="fa fa-gear"></i>
-          </a>
-          <h5>STYLE SWITCHER</h5>
-          <div class="theme-colours">
-            <p> Choose Colour style </p>
-            <ul>
-              <li>
-                <a href="#." class="defualt" id="defualt"></a>
-              </li>
-              <li>
-                <a href="#." class="red" id="red"></a>
-              </li>
-              <li>
-                <a href="#." class="green" id="green"></a>
-              </li>
-              <li>
-                <a href="#." class="orange" id="orange"></a>
-              </li>
-              <li>
-                <a href="#." class="purple" id="purple"></a>
-              </li>
-              <li>
-                <a href="#." class="yellow" id="yellow"></a>
-              </li>
-            </ul>
-          </div>
-          <div class="clr"> </div>
-        </div>
-
         <Header />
         <div class="revs">
           <Carousel
